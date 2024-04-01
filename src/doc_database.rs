f6d9c96b62b27d8167680c5fb229a7eda2621b14
BreezeShane <72027962+BreezeShane@users.noreply.github.com>
2024-04-01 08:04:05 +0800

    Finished Dataset Managing func.

--- src/doc_database.rs
+++ src/doc_database.rs
@@ -1,29 +1,109 @@
-use std::{fs::File, io::{BufRead, BufReader, Write}};
-
 use data_encoding::HEXUPPER;
 use serde::{Serialize, Deserialize};
+use std::{fs::File, io::{BufRead, BufReader, Write}};
 
-const DOC_STORED_PATH: &str = "./doc.db";
+const QUEUE_STORED_PATH: &str = "./queue.db";
+const DATASETS_STORED_PATH: &str = "./datasets.db";
 const QUEUE_MAX_LENGTH: usize = 10;
 
 #[derive(Serialize, Deserialize)]
 struct Dataset {
-    id: i8,
     name: String,
     timestamp: i64,
     available: bool
 }
-type Datasets = Vec<Dataset>;
+type DatasetVec = Vec<Dataset>;
 
-fn _load_data(){}
+// const DATASET_NONE_VALUE: Option<Dataset> = None;
 
-fn _write_data(){}
+trait DatasetTrait {
+    fn init_vec() -> DatasetVec;
+    fn load<
+        T: for<'a> Into< Option<&'a str> >
+        >(file_path: T) -> Result<DatasetVec, String>;
+    fn save(&self) -> Result<usize, std::io::Error>;
+    fn append(&mut self, dataset: Dataset) -> Result<(), String>;
 
-pub fn add_new_dataset(){}
+    fn search(vec: &mut DatasetVec, dataset_name: &str) -> Option<usize>;
+    fn en_disable(vec: &mut DatasetVec, dataset_name: &str) -> Result<bool, String>;
+    fn remove(vec: &mut DatasetVec, dataset_name: &str) -> Result<Dataset, String>;
+}
 
-pub fn enable_or_disable_dataset(){}
+impl DatasetTrait for DatasetVec {
+    fn init_vec() -> DatasetVec {
+        Vec::new()
+    }
 
-pub fn remove_dataset(){}
+    fn save(&self) -> Result<usize, std::io::Error> {
+        let json_encoded = HEXUPPER.encode(serde_json::to_string(self).unwrap().as_bytes());
+
+        let mut file = File::create(DATASETS_STORED_PATH).unwrap();
+        file.write(json_encoded.as_bytes())
+    }
+
+    fn append(&mut self, dataset: Dataset) -> Result<(), String> {
+        self.push(dataset);
+        Ok(())
+    }
+
+    fn load<
+        T: for<'a> Into< Option<&'a str> >
+    >(file_path: T) -> Result<DatasetVec, String>{
+        let file = 
+            File::open(
+                file_path.into()
+                .unwrap_or(DATASETS_STORED_PATH)
+            )
+            .map_err(|err| err.to_string())?;
+
+        let buffered = BufReader::new(file);
+
+        let mut data_loaded = String::new();
+        for data_stream in buffered.lines() {
+            let line = data_stream.map_err(|err| err.to_string())?;
+            data_loaded.push_str(line.as_str());
+        }
+
+        let data_vec_decoded = 
+            HEXUPPER.decode(data_loaded.as_bytes()).unwrap();
+        let data_string = 
+            String::from_utf8(data_vec_decoded).unwrap();
+        
+        let dataset_vec: DatasetVec = 
+            serde_json::from_str(&data_string.as_str())
+                .map_err(|err| err.to_string())?;
+        Ok(dataset_vec)
+    }
+
+    fn search(vec: &mut DatasetVec, dataset_name: &str) -> Option<usize> {
+        vec.iter().position(
+            |dataset| dataset.name == dataset_name
+        )
+    }
+
+    fn en_disable(vec: &mut DatasetVec, dataset_name: &str) -> Result<bool, String> {
+        let index_opt = Self::search(vec, dataset_name);
+        match index_opt {
+            None => return Err(format!("Dataset: {} does not exist!", dataset_name)),
+            Some(index) => {
+                let dataset = vec.get_mut(index).unwrap();
+                let new_status = !dataset.available;
+                dataset.available = new_status;
+                Ok(new_status)
+            }
+        }
+    }
+    
+    fn remove(vec: &mut DatasetVec, dataset_name: &str) -> Result<Dataset, String> {
+        let index_opt = Self::search(vec, dataset_name);
+        match index_opt {
+            None => return Err(format!("Dataset: {} does not exist!", dataset_name)),
+            Some(index) => return Ok(vec.remove(index))
+        }
+    }
+}
+
+
 
 /// Datasets ⮥
 /// 
@@ -31,7 +111,7 @@ pub fn remove_dataset(){}
 
 #[derive(Serialize, Deserialize, Clone)]
 pub struct TrainingUnit {
-    id: i8,
+    // id: i8,
     pic_path: String,
     label: String
 }
@@ -53,7 +133,7 @@ trait QueueTrait {
     fn append(&mut self, train_unit: TrainingUnit) -> Result<(), String>;
     fn pop(&mut self) -> Result<TrainingUnit, String>;
     fn save(&self) -> Result<usize, std::io::Error>;
-    fn load(&self) -> Result<Queue, String>;
+    fn load<T: for<'a> Into<Option<&'a str>>>(file_path: T) -> Result<Queue, String>;
 }
 
 impl QueueTrait for Queue {
@@ -97,15 +177,19 @@ impl QueueTrait for Queue {
     fn save(&self)  -> Result<usize, std::io::Error> {
         let json_encoded = HEXUPPER.encode(serde_json::to_string(self).unwrap().as_bytes());
 
-        let mut file = File::create(DOC_STORED_PATH).unwrap();
+        let mut file = File::create(QUEUE_STORED_PATH).unwrap();
         file.write(json_encoded.as_bytes())
     }
 
-    fn load(&self) -> Result<Queue, String> {
-    
+    fn load<
+        T: for<'a> Into<Option<&'a str>>
+    >(file_path: T) -> Result<Queue, String> {
         let file = 
-            File::open(DOC_STORED_PATH)
-                .map_err(|err| err.to_string())?;
+            File::open(
+                file_path.into()
+                .unwrap_or(QUEUE_STORED_PATH)
+            )
+            .map_err(|err| err.to_string())?;
 
         let buffered = BufReader::new(file);
     
