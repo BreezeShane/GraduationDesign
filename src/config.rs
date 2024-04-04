4601a60575ab9e53bb4fb2b231f38547847d90cc
BreezeShane <72027962+BreezeShane@users.noreply.github.com>
2024-04-04 23:59:06 +0800

    Humanized the number of expirations.

--- src/config.rs
+++ src/config.rs
@@ -9,13 +9,13 @@ pub const QUEUE_MAX_LENGTH: usize = 10;
 pub const DATASETS_STORED_PATH: &str = "./datasets.db";
 
 // authenticator.rs
-pub const JWT_EXPIRATION: i64 = 3900;
+pub const JWT_EXPIRATION: i64 = 3600 + 300; // 1h + 5min
 
 // feedback.rs
-pub const FEEDBACK_EXPIRATION: i64 = 86400;
+pub const FEEDBACK_EXPIRATION: i64 = 3600 * 24 * 7; // 7 days
 
 // daemon.rs
-pub const TIMER_DURATION: u64 = 3600;
+pub const TIMER_DURATION: u64 = 3600; // 1h
 
 // main.rs + model_manage.rs
 pub const MODEL_STORED_PATH: &str = "./model/";
