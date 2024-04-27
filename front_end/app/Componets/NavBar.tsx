ba96111d3c396714f45741ce32376da8369b74f7
BreezeShane <72027962+BreezeShane@users.noreply.github.com>
2024-04-27 20:02:29 +0800

    Removed unused dependencies in NavBar.tsx.

--- front_end/app/Componets/NavBar.tsx
+++ front_end/app/Componets/NavBar.tsx
@@ -1,6 +1,5 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
-import type { MenuProps } from 'antd';
-import { Menu, notification } from 'antd';
+import React, { useCallback, useState } from 'react';
+import { notification } from 'antd';
 import SignInButton from './Buttons/SignInButton';
 import SignUpButton from './Buttons/SignUpButton';
 import SignOutButton from './Buttons/SignOutButton';
@@ -21,26 +20,6 @@ const NavBar: React.FC = () => {
   const changeStatus = useCallback((signStatus: boolean) => {
     setSignStatus(signStatus);
   }, []);
-  // let items = [
-  //   signStatus ? { key: 'sign_out', label: (<SignOutButton signStatus={signStatus} changeStatus={changeStatus} messageClient={api} />)} : null,
-  //   !signStatus ? { key: 'sign_up', label: (<SignUpButton signStatus={signStatus} changeStatus={changeStatus} messageClient={api} />)} : null,
-  //   !signStatus ? { key: 'sign_in', label: (<SignInButton signStatus={signStatus} changeStatus={changeStatus} messageClient={api} />)} : null,
-  // ];
-  // useEffect(() => {
-  //   items = [
-  //     signStatus ? { key: 'sign_out', label: (<SignOutButton signStatus={signStatus} changeStatus={changeStatus} messageClient={api} />)} : null,
-  //     !signStatus ? { key: 'sign_up', label: (<SignUpButton signStatus={signStatus} changeStatus={changeStatus} messageClient={api} />)} : null,
-  //     !signStatus ? { key: 'sign_in', label: (<SignInButton signStatus={signStatus} changeStatus={changeStatus} messageClient={api} />)} : null,
-  //   ];
-  // }, [signStatus]);
-  // const items = [
-  //   { key: 'sign_out', label: (<SignOutButton isVisible={signStatus} signStatus={signStatus} changeStatus={changeStatus} messageClient={api} />)},
-  //   { key: 'sign_up', label: (<SignUpButton isVisible={!signStatus} signStatus={signStatus} changeStatus={changeStatus} messageClient={api} />)},
-  //   { key: 'sign_in', label: (<SignInButton isVisible={!signStatus} signStatus={signStatus} changeStatus={changeStatus} messageClient={api} />)}
-  // ];
-  
-
-  // console.log(signStatus, items);
 
   return (
     <div style={{
