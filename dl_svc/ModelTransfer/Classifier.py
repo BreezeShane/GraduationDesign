01316b5c44acc59b6328420f3839f2ec0a84c6ee
BreezeShane <72027962+BreezeShane@users.noreply.github.com>
2024-05-18 14:46:19 +0800

    Reconstructed dl_svc proj.

---
+++ dl_svc/ModelTransfer/Classifier.py
@@ -0,0 +1,8 @@
+
+
+class Classifier:
+    def __init__(pretrained_model_path: str):
+        pass
+
+    def forward():
+        pass
--- dl_svc/procedures/prune_model.py
+++
@@ -1,3 +0,0 @@
-"""
-    Prune Model Procedure Definition.
-"""
