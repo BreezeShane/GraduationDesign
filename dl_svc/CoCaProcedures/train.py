Fixed up the bug (Unable to save the last model). And corrected the submodel training acc computation.

dl_svc/CoCaProcedures/train.py
@@ -1,17 +1,15 @@
 """
     Training CoCa Vit Definition.
 """
-import random
 from os.path import join
 import torch
 import numpy as np
 import deepspeed
 from tqdm import tqdm
 from torchinfo import summary
-from torch.nn import CrossEntropyLoss
 from torch.utils.tensorboard import SummaryWriter
 from torch.optim.lr_scheduler import CosineAnnealingLR, CosineAnnealingWarmRestarts
-from peft import get_peft_config, get_peft_model, LoraConfig, TaskType
+from peft import get_peft_model, LoraConfig, TaskType
 
 from DataProcess.datasetloader import load_dataset
 from CoCa.coca_model import coca_vit_b_32, coca_vit_l_14
@@ -21,12 +19,6 @@ from Loss.contrastive_loss_with_temperature import ContrastiveLossWithTemperatur
 from Utils.early_stop import EarlyStopping
 from Utils.random_seed import setup_seed
 from config import TRAIN_CFG, TENSORBOARD_DATA_PATH, CHECKPOINT_PATH
-
-def embedding_cosine_similarity(matrix_1, matrix_2):
-    x = matrix_1.mul(matrix_2)
-    x = (x - x.min()) / (x.max() - x.min())
-    x = (x - 0.5) * 2
-    return x
 
 def train(args, carry_on=False):
     """ Train CoCa Vit Model. """
@@ -167,11 +159,11 @@ def train(args, carry_on=False):
 
             for name, param in model.named_parameters():
                 if param.grad is not None:
-                  writer.add_histogram(tag=name+'.grad', values=param.grad, global_step=idx)
+                    writer.add_histogram(tag=name+'.grad', values=param.grad, global_step=idx)
                 writer.add_histogram(tag=name+'.data', values=param.data, global_step=idx)
 
             len_t_dataloader = len(t_dataloader)
-            if idx > 0 and (idx % (len_t_dataloader // 10) == 0 or idx == len_t_dataloader):
+            if idx > 0 and idx % (len_t_dataloader // 10) == 0:
                 #=====================valid============================
                 if v_dataloader is not None:
                     valid_epoch_loss = []
@@ -243,6 +235,12 @@ def train(args, carry_on=False):
                     )
 
 
+        torch.save({
+                'name': model_name,
+                'model': model.state_dict()
+            },
+            join(args.mod_path, 'the-last-model.pt')
+        )
         t_loss_avg = np.average(train_epoch_loss)
         train_epochs_loss.append(t_loss_avg)
         print("\n----------------------------------------")

dl_svc/ModelTransfer/Classifier.py
@@ -4,7 +4,8 @@ from Layers.attention_pooler import AttentionPooler
 from Encoder.vision_transformer import vision_transformer
 
 class Classifier(nn.Module):
-    def __init__(pretrained_model_path: str, device, model_type="normal"):
+    def __init__(self, pretrained_model_path: str, device, model_type="normal"):
+        super().__init__()
         self.model_type = model_type.lower()
         self.device = device
         self.pretrained_model = vision_transformer(
@@ -26,17 +27,32 @@ class Classifier(nn.Module):
             patch_drop_rate=None,
         )
         self.attention_pooler = AttentionPooler(
-            input_embed_dim=512,
+            input_embed_dim=768,
             output_embed_dim=102,
-            n_head=8,
+            n_head=6,
             n_queries=1,
             layer_norm_eps=1e-5,
         )
         self.softmax = nn.Softmax(dim=1)
 
-        assert self.model_type in ["normal", "lora", "deepspeed"]
+        assert self.model_type in ["whole", "normal", "lora", "deepspeed"]
 
         match self.model_type:
+            case "whole":
+                whole_state_dict = torch.load(pretrained_model_path, map_location=self.device)
+
+                pretrained_model_dict = self.pretrained_model.state_dict()
+                load_dict = { k:v for k, v in whole_state_dict.items() if k in pretrained_model_dict.keys()}
+                pretrained_model_dict.update(load_dict)
+
+                attn_pool_dict = self.attention_pooler.state_dict()
+                load_dict = { k:v for k, v in whole_state_dict.items() if k in attn_pool_dict.keys()}
+                attn_pool_dict.update(load_dict)
+
+                self.pretrained_model.load_state_dict(pretrained_model_dict)
+                self.attention_pooler.load_state_dict(attn_pool_dict)
+                self.pretrained_model.eval()
+                self.attention_pooler.eval()
             case "normal":
                 trained_model = torch.load(pretrained_model_path, map_location=self.device)['model']
                 model_dict = self.pretrained_model.state_dict()
@@ -57,8 +73,7 @@ class Classifier(nn.Module):
     def forward(self, x):
         with torch.no_grad():
             self.pretrained_model.eval()
-            x = self.pretrained_model(x)
+            x = self.pretrained_model(x).last_hidden_state
         x = self.attention_pooler(x)
-        x = x.squeeze(1)
         x = self.softmax(x)
         return x

dl_svc/TransferProcedures/train.py
@@ -1,15 +1,16 @@
 import torch
 import numpy as np
 from torch import nn
-from tqdm import tqdm
+from os.path import join
+from tqdm import tqdm, trange
 from torchinfo import summary
 from torch.utils.tensorboard import SummaryWriter
 from torch.optim.lr_scheduler import CosineAnnealingWarmRestarts
 
+from Utils.random_seed import setup_seed
 from ModelTransfer.Classifier import Classifier
 from DataProcess.datasetloader import load_classic_dataset
 from config import TRANSFER_TRAIN_CFG, CHECKPOINT_PATH, TENSORBOARD_DATA_PATH
-from Utils.random_seed import setup_seed
 
 def train(args):
     if None in (args.stset, args.svset, args.smodel):
@@ -35,31 +36,36 @@ def train(args):
 
     writer = SummaryWriter(TENSORBOARD_DATA_PATH)
     #TB Print Model
-    print(summary(model, [ (1, 3, 512, 512) ], dtypes=[torch.float], device="cpu"))
-    writer.add_graph(model, input_to_model=[
-        torch.rand(1, 3, 512, 512)
-    ])
+    print(summary(
+        model,
+        input_size=(1, 3, 512, 512),
+        dtypes=[torch.float],
+        device="cpu",
+        mode="eval"
+    ))
+    writer.add_graph(model, input_to_model=torch.rand(1, 3, 512, 512))
 
     model.to(args.device)
 
     train_epochs_loss = []
+    train_epochs_acc = []
     valid_epochs_loss = []
-    train_acc = []
-    val_acc = []
+    valid_epochs_acc = []
 
-    for epoch in range(TRANSFER_TRAIN_CFG.EPOCHS):
+    for epoch in trange(TRANSFER_TRAIN_CFG.EPOCHS, desc="Training Submodel Epoch: "):
         model.train()
         train_epoch_loss = []
         acc, nums = 0., 0
 
-        for idx, (labels, inputs) in enumerate(tqdm(t_dataloader, desc="Training Submodel: ", position=0, leave=True)):
+        for idx, (labels, inputs) in enumerate(tqdm(t_dataloader, desc="Training Submodel: ")):
             inputs = inputs.to(args.device)
             labels = labels.to(args.device)
 
             optimizer.zero_grad()
             outputs = model(inputs)
-            predicts = outputs.squeeze(1).argmax(1)
-            loss = loss_criterion(predicts, labels)
+            predicts = outputs.argmax(1)
+            loss = loss_criterion(predicts.float(), labels.float())
+            loss.requires_grad_(True)
             loss.backward()
             optimizer.step()
             lr_scheduler.step()
@@ -80,56 +86,55 @@ def train(args):
 
             for name, param in model.named_parameters():
                 if param.grad is not None:
-                  writer.add_histogram(tag=f'submodel.{name}.grad', values=param.grad, global_step=idx)
+                    writer.add_histogram(tag=f'submodel.{name}.grad', values=param.grad, global_step=idx)
                 writer.add_histogram(tag=f'submodel.{name}.data', values=param.data, global_step=idx)
 
         len_t_dataloader = len(t_dataloader)
-        if idx > 0 and (idx % (len_t_dataloader // 10) == 0 or idx == len_t_dataloader):
-            #=====================valid============================
-            valid_epoch_loss = []
-            v_acc, v_nums = 0., 0
-            with torch.no_grad():
-                model.eval()
-                for vidx, (texts, inputs) in enumerate(tqdm(v_dataloader, desc="Validating submodel: ", position=0, leave=True)):
-                    inputs = inputs.to(args.device)
-                    texts = texts.to(args.device)
+        #=====================valid============================
+        valid_epoch_loss = []
+        v_acc, v_nums = 0., 0
+        with torch.no_grad():
+            model.eval()
+            for vidx, (labels, inputs) in enumerate(tqdm(v_dataloader, desc="Validating submodel: ")):
+                inputs = inputs.to(args.device)
+                labels = labels.to(args.device)
 
-                    outputs = model(inputs)
-                    predicts = outputs.squeeze(1).argmax(1)
-                    loss = loss_criterion(predicts, labels)
+                outputs = model(inputs)
+                predicts = outputs.argmax(1)
+                loss = loss_criterion(predicts.float(), labels.float())
 
-                    valid_epoch_loss.append(loss.item())
-                    v_acc += sum(predicts == labels).cpu()
-                    v_nums += labels.shape[0]
-                    #TB Print valid loss
-                    writer.add_scalar(f"Submodel_V_Loss_BS_{idx}_Epoch_{epoch+1}", loss.item(), vidx)
-            v_loss_avg = np.average(valid_epoch_loss)
-            v_avg_acc = 100 * v_acc / v_nums
-            valid_epochs_loss.append(v_loss_avg)
-            print("\n----------------------------------------------------------------------------------")
-            print(f"| Epoch: {epoch+1} Iters: {idx}, Valid Acc: {v_avg_acc:.3f}%, Avg Loss: {v_loss_avg} |")
-            print("----------------------------------------------------------------------------------\n")
+                valid_epoch_loss.append(loss.item())
+                v_acc += sum(predicts == labels).cpu()
+                v_nums += labels.shape[0]
+                #TB Print valid loss
+                writer.add_scalar(f"Submodel_V_Loss_BS_{vidx}_Epoch_{epoch+1}", loss.item(), vidx)
+        v_loss_avg = np.average(valid_epoch_loss)
+        v_avg_acc = 100 * v_acc / v_nums
+        valid_epochs_loss.append(v_loss_avg)
+        valid_epochs_acc.append(v_avg_acc)
+        print("\n----------------------------------------------------------------------------------")
+        print(f"| Epoch: {epoch+1}, Valid Acc: {v_avg_acc:.3f}%, Avg Loss: {v_loss_avg} |")
+        print("----------------------------------------------------------------------------------\n")
 
-            torch.save({
-                    'model': model.state_dict(),
-                    'opt': optimizer.state_dict(),
-                    'scheduler': lr_scheduler.state_dict()
-                },
-                join(
-                    CHECKPOINT_PATH,
-                    'common_submodel_checkpoint.pth'
-                )
+        torch.save({
+                'model': model.state_dict(),
+                'opt': optimizer.state_dict(),
+                'scheduler': lr_scheduler.state_dict()
+            },
+            join(
+                CHECKPOINT_PATH,
+                'common_submodel_checkpoint.pth'
             )
+        )
 
-            torch.save(model.state_dict(),
-                join(
-                    args.mod_path,
-                    f'{epoch+1}-{idx}-{len_t_dataloader}-submodel.pt'
-                )
-            )
+        torch.save(model.state_dict(), join(
+            args.mod_path,
+            f'{epoch+1}-{len_t_dataloader}-submodel.pt'
+        ))
         t_loss_avg = np.average(train_epoch_loss)
         t_acc = 100 * acc / nums
         train_epochs_loss.append(t_loss_avg)
+        train_epochs_acc.append(t_acc)
         print("\n------------------------------------------------------------------------")
         print(f"| Epoch {epoch+1}, Latest Train Acc = {t_acc:.3f}%, Loss = {t_loss_avg} |")
         print("------------------------------------------------------------------------\n")